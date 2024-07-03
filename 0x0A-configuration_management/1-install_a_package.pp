f7a04b788aa1cf22d5e515404f21e3a5ef902564
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-03 15:41:57 -0700

    Installing packages on remote servers

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,16 @@
 #Installing packages on remote server using puppet tool
 
-package {'Python 3.8':
+package { 'Python 3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
 
-package {'Flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package {'Werkzeug':
+package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
   require  => package['Flask'],
