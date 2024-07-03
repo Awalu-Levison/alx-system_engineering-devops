8702bea6aa3f93b2a409bc0f1e20e91f8fc2b573
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-03 15:14:18 -0700

    Firsty task of puppet tool

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Working with pupet server configuration tool
+
+file {'Awalu_puppet':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
