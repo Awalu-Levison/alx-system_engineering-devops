5f01d275449868362877093d7a598ff5fa7e2ce8
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-08 13:49:01 -0700

    Configure server to use without password using puppet tool

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # Configuring the server to use ssh connection using puppet configuration tool
 file {'/etc/ssh/ssh_config':
-  ensure  =>  'present',
+  ensure  => 'present',
 }
 
 file_line {'Turn off password auth':
-  path    =>  'PasswordAuthentication no',
-  line    =>  'PasswordAuthentication no',
-  match   =>  'PasswordAuthentication yes',
-  replace =>  'true',
+  path    => 'PasswordAuthentication no',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
 }
 
 file_line {'Using a private key':
-  path    =>  '/etc/ssh/ssh_config',
-  line    =>  'IdentityFile ~/.ssh/school',
-  match   =>  '^IdentityFile',
-  ensure  =>  'present',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  matc   => '^IdentityFile',
 }
