031f1f6fa08f40c32267e47cb506a78a0f4c0f78
Awalu-Levison <levisonawalu251@gmail.com>
2024-07-13 08:00:32 -0700

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,20 @@
-# Set up new ubuntu server without using the bash
-# with nginx with puppet configuration management tool
+# Setup New Ubuntu server with nginx
 
 exec { 'update system':
-	command => '/usr/bin/apt-get update',
+        command => '/usr/bin/apt-get update',
 }
-package { 'nginx server':
+
+package { 'nginx':
 	ensure => 'installed',
 	require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-	ensure => 'present',
-	path => '/etc/nginx/sites-available/default',
 	content => 'Hello World!'
 }
 
 exec {'redirect_me':
-	command => 'sed -i "24i\ rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
