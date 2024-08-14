94dd6fe19e5fe03d6378141f2ef5488f0af68243
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-14 15:12:14 -0700

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+gather employee data from API
+'''
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com"
+
+    userID = sys.argv[1]
+
+    user = requests.get(url + "users/{}".format(userID)).json()
+
+    username = user.get("username")
+    usage_param = {"userID": userID}
+    todos = requests.get(url + "todos", usage_param=usage_param)
+
+    data_to_export = {userId: []}
+
+    for todo in todos:
+        data_format = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+                }
+
+        data_to_export[userID].append(data_format)
+
+    with open("{}.json".format(userID), "w") as json_file:
+        json.dump(data_to_export, json_file, indent=4)
