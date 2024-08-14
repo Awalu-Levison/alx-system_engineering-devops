d0e3207c8ce31e7b628b0b69baca367fac6ea22c
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-13 21:07:37 -0700

    Export data to CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,23 +4,28 @@
 import csv
 import requests
 import sys
+"""Modules imported for usage"""
 
 if __name__ == '__main__':
     """Get system parameters"""
     user = sys.argv[1]
     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
     user_requ = requests.get(url_user)
-    """ANYTHING"""
+
+    """Employee data"""
     user_name = user_requ.json().get('username')
     task = url_user + '/todos'
     user_requ = requests.get(task)
     tasks = user_requ.json()
-    """export the file to csv as csvfile"""
+
+    """Export data to csv format"""
     with open('{}.csv'.format(user), 'w') as csvfile:
         for task in tasks:
             completed = task.get('completed')
             """Complete"""
             title_task = task.get('title')
-            """Done"""
             csvfile.write('"{}","{}","{}","{}"\n'.format(
                 user, user_name, completed, title_task))
+
+except Exception as e:
+    print(f"Error occured: {e}")
