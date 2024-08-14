8989a2e0a02f911814f065a3663e0fb5fb0415d6
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-13 20:28:20 -0700

    Export data to CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#1/usr/bin/python3
+"""Export data to CSV"""
+
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    """Get system parameters"""
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    user_requ = requests.get(url_user)
+    """ANYTHING"""
+    user_name = user_requ.json().get('username')
+    task = url_user + '/todos'
+    user_requ = requests.get(task)
+    tasks = user_requ.json()
+    """export the file to csv as csvfile"""
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """Complete"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
