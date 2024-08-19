3d99176e2d6d79c69a276c52db3409a0cbdd6e88
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-19 11:03:38 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix internal HTTP/1.0 500 Internal Server Error
 # Correct mistype .phpp files to .php fils
-exec {'fix code typos':
-    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-    path    => 'usr/bin/:bin'
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
