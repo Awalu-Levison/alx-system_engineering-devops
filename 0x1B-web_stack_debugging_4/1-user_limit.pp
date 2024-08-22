eff8fdef9f9546bb1b84e5ba5c9883a954d98fe5
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-22 20:28:16 +0000

    Add new user to the server

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
-# Change the OS configuration so that it is possible
-# to login with the holberton without errors
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
 
-exec {'OS security change file':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.config',
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
   path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
