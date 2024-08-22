1528bb9e080ad7f5c37d97c7dab59186b64b1b27
Awalu-Levison <levisonawalu251@gmail.com>
2024-08-22 20:06:47 +0000

    Add new user to the server

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
-# Add holberton as a new user
-# by modifying the configuration files
+# Modify the OS configuration to add holberton as new user
 
 exec {'Modify security system config file':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.config',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin'
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
